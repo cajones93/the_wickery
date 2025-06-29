6f17bed63465a06997bff063da84b6b30fc13fb1
Callum <callumjones93@hotmail.com>
2025-06-29 20:31:33 +0100

    added basic stripe functionality

--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -182,4 +182,3 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-

4035488dfbff62b4bccab3e200564eaa8eab7889
Callum <callumjones93@hotmail.com>
2025-06-29 12:25:07 +0100

    replaced calc_subtotal with calculation in contexts.py

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -3,6 +3,7 @@ from django import template
 
 register = template.Library()
 
+# obsolete due to calculations in contexts.py
 @register.filter(name='calc_subtotal')
-def calc_subtotal(price, quantity):
-    return price * quantity
+def calc_subtotal(price, quantity, size_modifier, wax_modifier):
+    return (price * size_modifier * wax_modifier) * quantity

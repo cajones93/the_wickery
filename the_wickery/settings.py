6bdc668e8c34eb1eb1dda184ca3f8020f3a3b4d8
Callum <callumjones93@hotmail.com>
2025-07-14 20:44:21 +0100

    added * allowed host

--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -31,7 +31,8 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1',  # Local preview
                  'localhost',  # Listen for stripe webhooks
-                 'the-wickery-d8e65af69641.herokuapp.com',  # Heroku app
+                 'the-wickery-d8e65af69641.herokuapp.com',
+                 '*', # Heroku app
                  ]
 
 

cbe380d44b9da1ea0c3266eb200c5799b61cc54c
Callum <callumjones93@hotmail.com>
2025-07-19 14:13:55 +0100

    added some responvieness changes at small screen sizes

--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -33,6 +33,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['127.0.0.1',  # Local preview
                  'localhost',  # Listen for stripe webhooks
                  'the-wickery-d8e65af69641.herokuapp.com',  # Heroku app
+                 '.herokuapp.com',
                  ]
 
 

410fdb857e3573415c41b9e252f908c3b2024384
Callum <callumjones93@hotmail.com>
2025-07-19 16:42:22 +0100

    updated django to v4.2.14

--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -232,8 +232,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-    EMAIL_SSL_KEYFILE = ''
-    EMAIL_SSL_CERTFILE = ''
 
 
 # Default primary key field type

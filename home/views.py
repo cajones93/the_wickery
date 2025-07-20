ab7b223902889617f268950950b089994f31d3e5
Callum <callumjones93@hotmail.com>
2025-07-20 22:28:52 +0100

    added features screenshots to readme

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to return the index page """
-
+    
     return render(request, 'home/index.html')
 
 
--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = False
+
 
 ALLOWED_HOSTS = ['127.0.0.1',  # Local preview
                  'localhost',  # Listen for stripe webhooks

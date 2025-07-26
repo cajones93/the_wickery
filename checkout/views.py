3091db420e94224a71aef78aaa3ccb7935cb5ff6
Callum <callumjones93@hotmail.com>
2025-07-26 23:12:13 +0100

    changed stripe payment intent to send boolean

--- checkout/views.py
+++ checkout/views.py
@@ -22,7 +22,7 @@ def cache_checkout_data(request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
+            'save_info': 'save_info' in request.POST,
             'username': request.user,
         })
         return HttpResponse(status=200)

0d1e93843b0d298d675617fe151b6ea79d78ac14
Callum <callumjones93@hotmail.com>
2025-07-27 21:44:12 +0100

    set debug to false

--- the_wickery/settings.py
+++ the_wickery/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',  # Local preview
                  'localhost',  # Listen for stripe webhooks

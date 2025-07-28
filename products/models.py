9171eb66e097a585876dc6caa1f42ff0df97461e
Callum <callumjones93@hotmail.com>
2025-07-28 22:36:02 +0100

    updated mobile header spacing

--- products/models.py
+++ products/models.py
@@ -72,7 +72,7 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_scents = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=1, null=True, blank=True, validators=[
+    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True, validators=[
             MinValueValidator(1, message='Rating must be at least 1.'),
             MaxValueValidator(5, message='Rating cannot exceed 5.')
         ])
